361ac4e2070ee5454cd4a808cabbc2e857180cda
luisricardo494 <luisricardo494@gmail.com>
2020-11-11 16:51:19 -0500

    build to heroku

--- backend/server.js
+++ backend/server.js
@@ -50,8 +50,10 @@ app.get('/config/paypal', (req, res) => {
 });
 
 app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../frontend/build/index.html'));
+});
 
 
-
-
-app.listen(port, () => {console.log("Server started at http://localhost:5001")});
+app.listen(port, () => {console.log(`Server started at ${port}`)});
